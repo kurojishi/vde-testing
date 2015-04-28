e2c02cea31495c07722d07096c769d9aa97d7c72
kurojishi <kurojishi@tapiri.org>
2015-04-28 16:23:06 +0200

    divied the server and client

--- flood.go
+++ flood.go
@@ -4,16 +4,20 @@ import (
 	"flag"
 	"fmt"
 	"net"
-	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
-func sendData(adr string) {
+func sendData(addr string, out string) {
 	var payload = []byte("skldjaslkcjlak")
-	conn, err := net.Dial("tcp", adr)
+	localAddr, err := net.ResolveTCPAddr("tcp", out)
+	if err != nil {
+		fmt.Print(err)
+	}
+	dialer := net.Dialer{LocalAddr: localAddr}
+	conn, err := dialer.Dial("tcp", addr)
 	if err != nil {
 		fmt.Print(err)
 	}
@@ -26,12 +30,21 @@ func sendData(adr string) {
 
 func readPackets(inter string, finished chan bool) {
 	handle, err := pcap.OpenLive(inter, 1600, true, 0)
+	defer handle.Close()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	packetSource := gopacket.NewPacketSource(handle, layers.LinkTypeEthernet)
-	for packet := range packetSource.Packets() {
+	packetSource.NoCopy = true
+	fmt.Println("catcher ready")
+	for i := 0; i < 20; i++ {
+		packet, err := packetSource.NextPacket()
+
+		if err != nil {
+			fmt.Print(err)
+		}
 		fmt.Println(packet.Dump())
+		fmt.Println(i)
 	}
 	finished <- true
 }
@@ -45,7 +58,7 @@ func receiveData(conn net.Listener, ok chan bool) {
 	}
 	for i := 0; i < 20; i++ {
 		_, err := listenerConnection.Read(payload)
-		fmt.Print(payload)
+		fmt.Println(string(payload))
 		if err != nil {
 			fmt.Print(err)
 		}
@@ -57,29 +70,31 @@ func receiveData(conn net.Listener, ok chan bool) {
 
 func main() {
 	var in = flag.String("in", "192.168.4.1:5000", "address to send the data too")
-	//var out = flag.String("out", "192.168.4.2:5000", "address to send the data from")
-	//outAddress := net.ParseIP(*out)
-	//if outAddress == nil {
-	//log.Fatalln("bad output ip address")
-	//}
-	listener, err := net.Listen("tcp", *in)
-	defer listener.Close()
-	if err != nil {
-		fmt.Print(err)
-	}
-	fmt.Println("server started")
-	ok := make(chan bool)
-	finished := make(chan bool)
-	go readPackets("tap0", finished)
-	time.Sleep(time.Duration(1000))
-	go receiveData(listener, ok)
-	go sendData(*in)
-	x := <-ok
-	if !x {
-		fmt.Println("WTF")
-	}
-	y := <-finished
-	if !y {
-		fmt.Println("WTF")
+	var out = flag.String("out", "192.168.4.15", "address to send the data from")
+	var sender = flag.Bool("sender", false, "service will be a server")
+	if !*sender {
+		listener, err := net.Listen("tcp", *in)
+		defer listener.Close()
+		if err != nil {
+			fmt.Print(err)
+		}
+		fmt.Println("server started")
+		ok := make(chan bool)
+		finished := make(chan bool)
+		go readPackets("tap0", finished)
+		if err != nil {
+			fmt.Print(err)
+		}
+		go receiveData(listener, ok)
+		y := <-finished
+		if !y {
+			fmt.Println("WTF")
+		}
+		x := <-ok
+		if !x {
+			fmt.Println("WTF")
+		}
+	} else {
+		go sendData(*in, *out)
 	}
 }
