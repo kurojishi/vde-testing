badc8a69b41cc01625bfeec3cff4d053c3c71d4a
kurojishi <kurojishi@tapiri.org>
2015-06-03 16:40:37 +0200

    PoolStats

---
+++ pstats.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"github.com/kurojishi/procfs"
+)
+
+//PollVdeStats save stats gather information
+//on on the pid process using the proc filesystem
+func PollVdeStats(pid int, stop chan bool) error {
+	process, err := procfs.NewProcess(pid, true)
+	if err != nil {
+		return err
+	}
+	pstats, err := process.Stat()
+	if err != nil {
+		return err
+	}
+	pstatsm, err := process.Statm()
+	if err != nil {
+		return err
+	}
+	pstatus, err := process.Status()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Polling vde_switch data: cputime: %v memory: %v context_switches: %v threads: %v", pstats.Utime.UnixNano()+pstats.Stime.UnixNano(), pstatsm.Size, pstatus.NVcswitch+pstatus.Vcswitch, pstats.NumThreads)
+
+	return nil
+}
