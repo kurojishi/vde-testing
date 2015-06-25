6b19348ca0d7bdda202ad4649e9a725f0a435c97
kurojishi <kurojishi@tapiri.org>
2015-06-25 19:12:41 +0200

    removed client.go and created streestest.go to be converted as a Test

--- server.go
+++ server.go
@@ -1,15 +1,5 @@
 package vdetesting
 
-import (
-	"log"
-	"net"
-	"strconv"
-	"sync"
-	"time"
-
-	"github.com/kurojishi/vdetesting/utils"
-)
-
 const (
 	bandwidth  int32 = 1
 	latency    int32 = 2
@@ -23,67 +13,3 @@ const (
 	stop  int32 = 1
 	ready int32 = 2
 )
-
-func signalLoop(control string, cch chan int32) {
-	for msg := range cch {
-		err := utils.SendControlSignal(control, msg)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func manageConnections(address string, sch chan int32, wg sync.WaitGroup) {
-	listener, err := net.Listen("tcp", address)
-	if err != nil {
-		log.Fatalf("Manage Connections error: %v", err)
-	}
-	wg.Add(1)
-	defer wg.Done()
-	//defer listener.Close()
-	for {
-		select {
-		case <-sch:
-			return
-		default:
-			conn, err := listener.Accept()
-			//defer conn.Close()
-			if err != nil {
-				log.Fatalf("Manage Connections error 2: %v", err)
-			}
-			go utils.DevNullConnection(conn, &wg)
-
-		}
-	}
-	log.Printf("Closing connection %v", address)
-
-}
-
-//StressTest lauch a test to see what the vde_switch will do on very intensive traffic
-func StressTest(address string, startingPort int, cch chan int32, pid int) {
-	log.Print("Starting stress test")
-	schContainer := make([]chan int32, 0, 50)
-	//ticker, sch := PollStats(pid, "stress")
-	var wg sync.WaitGroup
-	for i := 0; i < 50; i++ {
-		ssch := make(chan int32, 1)
-		finalAddr := address + ":" + strconv.Itoa(startingPort+i)
-		go manageConnections(finalAddr, ssch, wg)
-		schContainer = append(schContainer, ssch)
-
-	}
-	cch <- stress
-	timer := time.NewTimer(1 * time.Minute)
-	<-timer.C
-	log.Print("timer elapsed")
-	cch <- stressStop
-	for i := 0; i < len(schContainer); i++ {
-		schContainer[i] <- stop
-	}
-	//ticker.Stop()
-	log.Print("Stopping ticker")
-	//sch <- true
-	log.Print("Asking Threads to stop")
-	wg.Wait()
-	log.Print("Finished stress test")
-}
---
+++ stresstest.go
@@ -0,0 +1,95 @@
+package vdetesting
+
+import (
+	"log"
+	"math/rand"
+	"net"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/kurojishi/vdetesting/utils"
+)
+
+var sizes = []int64{10, 30, 50, 100, 200, 500}
+
+func manageConnections(address string, sch chan int32, wg sync.WaitGroup) {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Manage Connections error: %v", err)
+	}
+	wg.Add(1)
+	defer wg.Done()
+	//defer listener.Close()
+	for {
+		select {
+		case <-sch:
+			return
+		default:
+			conn, err := listener.Accept()
+			//defer conn.Close()
+			if err != nil {
+				log.Fatalf("Manage Connections error 2: %v", err)
+			}
+			go utils.DevNullConnection(conn, &wg)
+
+		}
+	}
+	log.Printf("Closing connection %v", address)
+
+}
+
+//StressTest lauch a test to see what the vde_switch will do on very intensive traffic
+func StressTest(address string, startingPort int, cch chan int32, pid int) {
+	log.Print("Starting stress test")
+	schContainer := make([]chan int32, 0, 50)
+	//ticker, sch := PollStats(pid, "stress")
+	var wg sync.WaitGroup
+	for i := 0; i < 50; i++ {
+		ssch := make(chan int32, 1)
+		finalAddr := address + ":" + strconv.Itoa(startingPort+i)
+		go manageConnections(finalAddr, ssch, wg)
+		schContainer = append(schContainer, ssch)
+
+	}
+	cch <- stress
+	timer := time.NewTimer(1 * time.Minute)
+	<-timer.C
+	log.Print("timer elapsed")
+	cch <- stressStop
+	for i := 0; i < len(schContainer); i++ {
+		schContainer[i] <- stop
+	}
+	//ticker.Stop()
+	log.Print("Stopping ticker")
+	//sch <- true
+	log.Print("Asking Threads to stop")
+	wg.Wait()
+	log.Print("Finished stress test")
+}
+
+//composition of sendData
+func stressSend(address string, startingPort int) []chan int32 {
+	controlChannels := make([]chan int32, 0, 50)
+	for i := 0; i < 50; i++ {
+		ssch := make(chan int32)
+		controlChannels = append(controlChannels, ssch)
+
+		finalAddr := address + ":" + strconv.Itoa(startingPort+i)
+		go func(finalAddr string, ssch chan int32) {
+
+			for {
+				select {
+				case <-ssch:
+					return
+				default:
+					r := rand.New(rand.NewSource(time.Now().UnixNano()))
+					utils.SendData(finalAddr, sizes[r.Int31n(int32(len(sizes)-1))])
+				}
+			}
+		}(finalAddr, ssch)
+
+	}
+	return controlChannels
+
+}
