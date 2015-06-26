a7e253238fbde0b3403528612dff8d7898801982
kurojishi <kurojishi@tapiri.org>
2015-06-26 16:00:46 +0200

    moved things around and used waitcontrolmessage method instead of custom one

--- bandwidth.go
+++ bandwidth.go
@@ -2,7 +2,6 @@
 package vdetesting
 
 import (
-	"encoding/binary"
 	"log"
 	"net"
 
@@ -16,7 +15,6 @@ type BandwidthTest struct {
 	address net.Addr
 	port    Port
 	name    string
-	cch     chan int32
 	stats   StatManager
 	kind    string
 	pid     int
@@ -41,14 +39,13 @@ func NewBandwidthTest(kind string, iface string, address string, port int, pid i
 	bandwidth := BandwidthTest{iface: face,
 		address: addr,
 		port:    Port{port},
-		cch:     make(chan int32),
 		stats:   NewStatManager(),
 		kind:    kind,
 		pid:     pid,
 		name:    "bandwidth"}
 	if kind == "server" {
-		//tcpStat := NewTCPStat(bandwidth.iface, bandwidth.port, bandwidth.name)
-		//bandwidth.AddStat(&tcpStat)
+		tcpStat := NewTCPStat(bandwidth.iface, bandwidth.port, bandwidth.name)
+		bandwidth.AddStat(&tcpStat)
 		pstat := NewProfilingStat(bandwidth.pid, bandwidth.name)
 		bandwidth.AddStat(&pstat)
 	}
@@ -70,61 +67,9 @@ func (t *BandwidthTest) statisticsStop() {
 	t.stats.Stop()
 }
 
-//StartServer start the server side of Bandwidthtest
-func (t *BandwidthTest) StartServer() {
-	log.Printf("Starting bandwidth test")
-	bind, err := utils.InterfaceAddrv4(t.iface)
-	listener, err := net.Listen("tcp", bind+":"+t.port.String())
-	if err != nil {
-		log.Fatalf("ReceiveData %v", err)
-	}
-	defer listener.Close()
-	go utils.SendControlSignal(t.address.String(), 1)
-	t.statisticsStart()
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-	utils.DevNullConnection(conn, nil)
-	t.statisticsStop()
-	log.Print("Finished bandwidth test")
-
-}
-
 //IFace Return the Interface
 func (t *BandwidthTest) IFace() *net.Interface {
 	return t.iface
-}
-
-//StartClient start the TestClient side of this Test
-func (t *BandwidthTest) StartClient() error {
-	var arrived = false
-	local, err := utils.Localv4Addr()
-	if err != nil {
-		return err
-	}
-	clistener, err := net.Listen("tcp", local+":8999")
-	log.Printf("waiting control message for bandwidth test on %v", local+":8999")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for !arrived {
-		conn, err := clistener.Accept()
-		if err != nil {
-			return err
-		}
-		var buf int32
-		binary.Read(conn, binary.LittleEndian, &buf)
-		if buf == 1 {
-			arrived = true
-			clistener.Close()
-			log.Printf("control message arrived")
-		}
-	}
-	log.Print("control message arrived sending data")
-	utils.SendData(t.address.String()+":"+t.port.String(), 1000)
-	return nil
 }
 
 //Name return the name of this test
@@ -140,4 +85,36 @@ func (t *BandwidthTest) Port() Port {
 //Address return the IP address of the test
 func (t *BandwidthTest) Address() net.Addr {
 	return t.address
+}
+
+//StartServer start the server side of Bandwidthtest
+func (t *BandwidthTest) StartServer() {
+	log.Printf("Starting bandwidth test")
+	bind, err := utils.InterfaceAddrv4(t.iface)
+	listener, err := net.Listen("tcp", bind+":"+t.port.String())
+	if err != nil {
+		log.Fatalf("Could not bind to listener %v", err)
+	}
+	defer listener.Close()
+	go utils.SendControlSignal(t.address.String(), 1)
+	t.statisticsStart()
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	utils.DevNullConnection(conn, nil)
+	t.statisticsStop()
+	log.Print("Finished bandwidth test")
+
+}
+
+//StartClient start the TestClient side of this Test
+func (t *BandwidthTest) StartClient() {
+	err := utils.WaitForControlMessage(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("control message arrived sending data")
+	utils.SendData(t.address.String()+":"+t.port.String(), 1000)
 }
